425dcf2dbd770041a1faee86b8c2fcd3744d22ee
kvrivera <heykatrivera@gmail.com>
2025-01-21 14:18:02 -0500

    Individual ACtivity for week 02, finished Step 5

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,6 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
 const dataSource = new ProductData();
-const productListing = new ProductListing();
+const productListing = new ProductListing(dataSource);
 
 console.log(productListing);
